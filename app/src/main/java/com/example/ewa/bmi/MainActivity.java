f10d2d740ee67e17abeb50b5e9bd864876c2e549
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-05-06 20:44:56 +0200

    refactoring

--- app/src/main/java/com/example/ewa/bmi/MainActivity.java
+++ app/src/main/java/com/example/ewa/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ewa.bmi;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,14 +13,17 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final double WOMEN_LIQUID_PERCENT_VOLUME = 0.6;
+    public static final double MAN_LIQUID_PERCENT_VOLUME = 0.7;
+    public static final int GENDER_POSITION_MAN = 0;
+    public static final int GENDER_POSITION_WOMEN = 1;
+    public static final double NORMAL_BMI_BOTTOM_LIMIT = 18.5;
+    public static final double NORMAL_BMI_UPPER_LIMIT = 25.0;
+
     Spinner spinnerGender;
     Spinner spinnerActivity;
-    double liquid;
-    double percent, weight, height, age;
-    double ppm;
-    double cpm;
     double activityIndex;
-    boolean women;
+    boolean isWomen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +35,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 parent.getItemAtPosition(position);
-                if (position == 0) {
+                if (position == GENDER_POSITION_MAN) {
                     Toast.makeText(MainActivity.this, "Wybrano mężczyznę", Toast.LENGTH_SHORT).show();
-                    women = false;
-                } else if (position == 1) {
+                } else if (position == GENDER_POSITION_WOMEN) {
                     Toast.makeText(MainActivity.this, "Wybrano kobietę", Toast.LENGTH_SHORT).show();
-                    women = true;
                 }
 
                 calcBmi();
@@ -91,77 +93,156 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void tryCalcBmi() {
-        parseWeight();
 
-        parseHeight();
+        boolean isWomen = isWomen();
+        double weight = parseWeight();
+        double height = parseHeight();
+        double age = parseAge();
 
-        parseAge();
-
-        calcPPM();
-
-        double bmi = weight / (Math.pow(height, 2));
+        double ppm = calcPPM(isWomen, weight, height, age);
+        double cpm = calcCPM(ppm);
+        double bmi = calcBMI(weight, height);
 
 
-        String resultString = "Twoje BMI wynosi: " + String.format("%.2f", bmi) + "\n";
+        double weightToNormal = getWeightToNormal(bmi, height);
+        double liquid = calcLiquid(weight, weightToNormal);
 
-        if (bmi < 18.5) {
-            double underweight = (18.5 - bmi) * (Math.pow(height, 2));
-            liquid = (weight + underweight) * percent - 0.15 * underweight;
-            resultString = resultString + "Posiadasz niedowagę " + String.format("%.2f", underweight) + "kg" + "\n";
-        } else if (bmi >= 25) {
-            double overweight = (bmi - 25) * (Math.pow(height, 2));
-            liquid = (weight - overweight) * percent + 0.15 * overweight;
-            resultString += "Posiadasz nadwagę " + String.format("%.2f", overweight) + " kg" + "\n";
+        double squaredHeight = height * height;
+
+        double bmiOverweight = NORMAL_BMI_UPPER_LIMIT * squaredHeight;
+        double bmiUnderweight = NORMAL_BMI_BOTTOM_LIMIT * squaredHeight;
+
+        displayResults(ppm, cpm, bmi, liquid, weightToNormal, bmiOverweight, bmiUnderweight);
+
+    }
+
+    private double calcBMI(double weight, double height) {
+        return weight / (Math.pow(height, 2));
+    }
+
+    private void displayResults(double ppm, double cpm, double bmi, double liquid, double weightToNormal, double bmiOverweight, double bmiUnderweight) {
+        String overweightText = getOverweightText(bmiOverweight);
+        String underWeightText = createUnderweightText(bmiUnderweight);
+        String liquidValueText = getLiquidValueText(liquid);
+        String ppmText = createPPMText(ppm);
+        String cpmText = createCPMText(cpm);
+        String bmiResult = createBMIValueText(bmi);
+        String categoryText = createCategoryText(bmi, weightToNormal);
+
+        StringBuilder resultTextBuilder = new StringBuilder();
+
+        resultTextBuilder
+                .append(bmiResult).append("\n")
+                .append(categoryText).append("\n")
+                .append(overweightText).append("\n")
+                .append(underWeightText).append("\n")
+                .append(liquidValueText).append("\n\n")
+                .append("Zapotrzebowanie na energię:\n")
+                .append(ppmText)
+                .append(cpmText)
+        ;
+
+        setResultText(resultTextBuilder.toString());
+    }
+
+    @NonNull
+    private String createCPMText(double cpm) {
+        return " -  całtkowita przemiana materii (CPM) wynosi " + String.format("%.2f", cpm) + " kcal";
+    }
+
+    @NonNull
+    private String createPPMText(double ppm) {
+        return " - podstawowa przemiana materii (PPM) wynosi " + String.format("%.2f", ppm) + " kcal" + "\n";
+    }
+
+    @NonNull
+    private String createUnderweightText(double bmiUnderweight) {
+        return "Będziesz mieć niedowagę poniżej " + String.format("%.2f", bmiUnderweight) + " kg";
+    }
+
+    @NonNull
+    private String getOverweightText(double bmiOverweight) {
+        return "Będziesz mieć nadwagę powyżej " + String.format("%.2f", bmiOverweight) + " kg";
+    }
+
+    @NonNull
+    private String getLiquidValueText(double liquid) {
+        return "Ilość płynów ustrojowych to " + String.format("%.2f", liquid) + " kg";
+    }
+
+    @NonNull
+    private String createBMIValueText(double bmi) {
+        return "Twoje BMI wynosi: " + String.format("%.2f", bmi);
+    }
+
+    private String createCategoryText(double bmi, double weightToNormal) {
+        String categoryText;
+        if (bmi < NORMAL_BMI_BOTTOM_LIMIT) {
+            categoryText = "Posiadasz niedowagę " + String.format("%.2f", weightToNormal) + " kg";
+        } else if (bmi >= NORMAL_BMI_UPPER_LIMIT) {
+            categoryText = "Posiadasz nadwagę " + String.format("%.2f", -weightToNormal) + " kg";
         } else {
-            liquid = percent * weight;
-            resultString += "Posiadasz prawidłową wagę" + "\n";
+            categoryText = "Posiadasz prawidłową wagę";
         }
+        return categoryText;
+    }
 
-        double bmi_overweight = 25 * height * height;
-        resultString += "Będziesz mieć nadwagę przy " + String.format("%.2f", bmi_overweight) + " kg" + "\n";
+    private double getWeightToNormal(double bmi, double height) {
 
-        double bmi_underweight = 18.5 * height * height;
-        resultString += "Będziesz mieć niedowagę przy " + String.format("%.2f", bmi_underweight) + " kg" + "\n";
+        double squaredHeight = Math.pow(height, 2);
 
-        resultString += "Ilość płynów ustrojowych to " + String.format("%.2f", liquid) + " kg" + "\n \n";
+        return (getNormalBMIThresholdForBMI(bmi) - bmi) * squaredHeight;
+    }
 
-        resultString += "Zapotrzebowanie na energię: " + "\n";
+    public double getNormalBMIThresholdForBMI(double bmi) {
+        return Math.min(Math.max(bmi, NORMAL_BMI_BOTTOM_LIMIT), NORMAL_BMI_UPPER_LIMIT);
+    }
 
-        resultString += " - podstawowa przemiana materii (PPM) wynosi " + String.format("%.2f", ppm) + " kcal"+"\n";
-        cpm = ppm * activityIndex;
+    private double calcLiquid(double weight, double underWeightValue) {
+        return (weight + underWeightValue) * getLiquidPercentVolume(isWomen) - 0.15 * underWeightValue;
+    }
 
-        resultString += " -  całtkowita przemiana materii (CPM) wynosi " + String.format("%.2f", cpm) + " kcal";
+    private boolean isWomen() {
+        return spinnerGender.getSelectedItemId() == GENDER_POSITION_WOMEN;
+    }
+
+    private void setResultText(String resultText) {
         TextView resultTextView = (TextView) findViewById(R.id.result_text);
-        resultTextView.setText(resultString);
-
+        resultTextView.setText(resultText);
     }
 
-    private void calcPPM() {
-        if (women) {
-            percent = 0.6;
-            ppm = 665.09 + 9.56 * weight + 1.85 * height - 4.67 * age;
+    private double calcPPM(boolean isWomen, double weight, double height, double age) {
+        if (isWomen) {
+            return 665.09 + 9.56 * weight + 1.85 * height - 4.67 * age;
         } else {
-            percent = 0.7;
-            ppm = 66.47 + 13.7 * weight + 5 * 100 * height - 6.76 * age;
+            return 66.47 + 13.7 * weight + 5 * 100 * height - 6.76 * age;
         }
     }
 
-    private void parseAge() {
+    private double calcCPM(double ppm) {
+        return ppm * activityIndex;
+    }
+
+    private double getLiquidPercentVolume(boolean isWomen) {
+        return isWomen ? WOMEN_LIQUID_PERCENT_VOLUME : MAN_LIQUID_PERCENT_VOLUME;
+    }
+
+    private double parseAge() {
         EditText ageEditText = (EditText) findViewById(R.id.age_edit);
         String ageString = ageEditText.getText().toString();
-        age = Double.parseDouble(ageString);
+        return Double.parseDouble(ageString);
     }
 
-    private void parseHeight() {
+    private double parseHeight() {
         EditText heigthEditText = (EditText) findViewById(R.id.height_edit);
         String heightString = heigthEditText.getText().toString();
-        height = Double.parseDouble(heightString);
+        return Double.parseDouble(heightString);
     }
 
-    private void parseWeight() {
+    private double parseWeight() {
         EditText weightEditText = (EditText) findViewById(R.id.weight_edit);
         String weightString = weightEditText.getText().toString();
-        weight = Double.parseDouble(weightString);
+        return Double.parseDouble(weightString);
     }
 
 
